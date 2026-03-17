632e048f00d8c21623bcd9420cb85351f029a422
redouane <benbrahimredouane664@gmail.com>
2026-03-17 13:08:44 +0000

    chore: add login function in authcontroler

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class Authcontroller extends Controller
@@ -23,7 +23,7 @@ class Authcontroller extends Controller
 
 
         ]);
-        $token = $user->createtoken('auth_sanctum')->plienTextToken;
+        $token = $user->createtoken('auth_sanctum')->plainTextToken;
 
         return response()->json([
 
@@ -34,5 +34,33 @@ class Authcontroller extends Controller
             ],
             'message' => 'User registred succefully!',
         ], 201);
+    }
+
+    public function login(LoginRequest $request){
+
+    $validated = $request->validated();
+
+    $user = User::where('email',$validated['email'] )->first();
+
+    if(!$user || !Hash::check($validated['password'],$user->password)){
+        return response()->json([
+            'success'=>false,
+            'data'=>null,
+            'message'=>'The provided credentials are incorrect'
+
+        ], 401);
+    }
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+
+    'success'=> true,
+    'data'=> [
+        'user'=> $user,
+        'token'=> $token
+    ],
+    'message'=>'login successfuly!'
+
+    ], 200);
     }
 }
