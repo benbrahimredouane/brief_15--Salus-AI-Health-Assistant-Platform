ee7c20e8096d855be4f9916b0c2e09ab6b067173
redouane <benbrahimredouane664@gmail.com>
2026-03-18 14:25:12 +0000

    feat: update API routes to include SymptomController and fix logout/me route syntax

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SymptomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,14 +9,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+//auth routes
 Route::post('/register',[AuthController::class , 'register']);
 Route::post('/login',[AuthController::class, 'login']);
 
+//protected routes
 Route::middleware('auth:sanctum')->group(function(){
 
-Route::post('/logout',AuthController::class,'logout');
-Route::post('/me',AuthController::class,'me');
+Route::post('/logout',[AuthController::class,'logout']);
+Route::post('/me',[AuthController::class,'me']);
+//symptoms routes
+Route::apiResource('symptoms' , SymptomController::class);
 
 
 
