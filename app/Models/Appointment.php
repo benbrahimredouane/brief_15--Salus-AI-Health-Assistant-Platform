937a1257b4ebf902f1d3cd3d6647252017aa91f9
redouane <benbrahimredouane664@gmail.com>
2026-03-24 15:20:20 +0100

    feat: add fillable variable with appoitment relation

---
+++ app/Models/Appointment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    /** @use HasFactory<\Database\Factories\AppointmentFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'doctor_id',
+        'appointment_date',
+        'status',
+      
+    ];
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+}
